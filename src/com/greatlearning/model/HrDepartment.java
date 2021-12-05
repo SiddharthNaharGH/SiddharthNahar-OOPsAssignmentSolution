6c6faf60fe48190de6305f1265ae3bd032fe216e
SiddharthNaharGH <siddharthnahar@hotmail.com>
2021-12-05 22:43:48 +0530

    Solution to OOPS Assessment-1

---
+++ src/com/greatlearning/model/HrDepartment.java
@@ -0,0 +1,32 @@
+package com.greatlearning.model;
+
+public class HrDepartment extends SuperDepartment {
+
+	public HrDepartment() {
+
+	}
+
+	@Override
+	public String departmentName() {
+
+		return "Hr Department";
+	}
+
+	@Override
+	public String getTodaysWork() {
+
+		return "Fill today's timesheet and mark your attendance";
+	}
+
+	@Override
+	public String getWorkDeadline() {
+
+		return "Complete by EOD";
+	}
+	
+	public String doActivity()
+	{
+		return "team Lunch";
+	}
+
+}
